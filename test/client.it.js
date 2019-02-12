509f651d7ca090832feb44c1ae1452028e4b5907
Gil Amran <gilamran@gmail.com>
2019-02-12 23:02:44 +0200

    Fixed wrong usage of this in tests

--- test/client.it.js
+++ test/client.it.js
@@ -6,38 +6,36 @@ const TscWatchClient = require('../lib/client');
 const { driver: tscWatchDriver } = require('./driver.js');
 
 describe('Client Events', () => {
-  before(() => {
-    this.successSourcePath = './tmp/fixtures/passing.ts';
-    this.failSourcePath = './tmp/fixtures/failing.ts';
-  });
+  let watchClient;
+  let callback;
 
   beforeEach(() => {
-    this.watchClient = new TscWatchClient();
-    this.callback = sinon.stub();
+    watchClient = new TscWatchClient();
+    callback = sinon.stub();
   });
-  afterEach(() => this.watchClient.kill());
+  afterEach(() => watchClient.kill());
 
   describe('Events', () => {
     it('Should emit "first_success" on first success', () => {
-      this.watchClient.on('first_success', this.callback);
-      this.watchClient.start('--noClear', '--out', './tmp/output.js', './tmp/fixtures/passing.ts');
+      watchClient.on('first_success', callback);
+      watchClient.start('--noClear', '--out', './tmp/output.js', './tmp/fixtures/passing.ts');
 
-      return eventually(() => expect(this.callback.calledOnce).to.be.true);
+      return eventually(() => expect(callback.calledOnce).to.be.true);
     });
 
     it('Should fire "subsequent_success" on subsequent successes', () => {
-      this.watchClient.on('subsequent_success', this.callback);
-      this.watchClient.start('--noClear', '--out', './tmp/output.js', './tmp/fixtures/passing.ts');
+      watchClient.on('subsequent_success', callback);
+      watchClient.start('--noClear', '--out', './tmp/output.js', './tmp/fixtures/passing.ts');
       tscWatchDriver.modifyAndSucceedAfter(1500);
 
-      return eventually(() => expect(this.callback.calledOnce).to.be.true);
+      return eventually(() => expect(callback.calledOnce).to.be.true);
     });
 
     it('Should fire "compile_errors" on when tsc compile errors occur', () => {
-      this.watchClient.on('compile_errors', this.callback);
-      this.watchClient.start('--noClear', '--out', './tmp/output.js', './tmp/fixtures/failing.ts');
+      watchClient.on('compile_errors', callback);
+      watchClient.start('--noClear', '--out', './tmp/output.js', './tmp/fixtures/failing.ts');
 
-      return eventually(() => expect(this.callback.calledOnce).to.be.true);
+      return eventually(() => expect(callback.calledOnce).to.be.true);
     });
   });
 });
